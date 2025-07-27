8a8809adca31fcc16fea05be4ce0683304680089
BrandonTheProgram <BrandonEvans735@gmail.com>
2025-07-27 13:32:21 -0700

    Pokemon Paldea Level Up Implemented

--- restart-db.sh
+++ restart-db.sh
@@ -27,6 +27,7 @@ SQL_FILES=(
     "./db/Schemas/PokemonLevelUpMoveset-Kalos.sql"
     "./db/Schemas/PokemonLevelUpMoveset-Alola.sql"
     "./db/Schemas/PokemonLevelUpMoveset-Galar.sql"
+    "./db/Schemas/PokemonLevelUpMoveset-Paldea.sql"
 )
 
 # Remove the existing database file if it exists
