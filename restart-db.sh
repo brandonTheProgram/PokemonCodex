6c40620b1eb81c82206bf8c65ac7f04ccb814b8f
BrandonTheProgram <BrandonEvans735@gmail.com>
2025-09-17 22:22:45 -0700

    Pokemon Alola Technical Moves Implemented

--- restart-db.sh
+++ restart-db.sh
@@ -34,6 +34,7 @@ SQL_FILES=(
     "./db/Schemas/PokemonTechnicalMoveset-Sinnoh.sql"
     "./db/Schemas/PokemonTechnicalMoveset-Unova.sql"
     "./db/Schemas/PokemonTechnicalMoveset-Kalos.sql"
+    "./db/Schemas/PokemonTechnicalMoveset-Alola.sql"
 )
 
 # Remove the existing database file if it exists
